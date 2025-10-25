213926b309535db1d90d258ec1607fbdf126f61f
3002tad <dat451303@gmail.com>
2025-10-25 10:09:46 +0700

    exam

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -7,6 +7,7 @@ class ProductController {
     this.getProducts = this.getProducts.bind(this);
     this.createOrder = this.createOrder.bind(this);
     this.getOrderById = this.getOrderById.bind(this);
+    this.getProductById= this.getProductById.bind(this);
   }
   async createProduct(req, res) {
     try {
@@ -93,6 +94,19 @@ class ProductController {
     }
   }
 
+  async getProductById(req,res){
+    try{
+      const { id } = req.params;
+      const product = await this.productService.getProductById(id);
+      if(!product){
+        return res.status(404).json({message: "not found"});
+      }
+      res.status(200).json(product);
+    }catch(error){
+      console.error(error);
+      res.status(500).json({message: "server error"});
+    }
+  }
 
 }
 
